8652895004e4ba28e6600f837fa14cfef5d3f20b
sadmanfatin <sadmanfatin208@gmail.com>
2022-02-03 17:47:03 +0600

    second commit

--- testPage1.php
+++ testPage1.php
@@ -2,99 +2,84 @@
  set_time_limit(298);
 include 'dbConn.php';
 include 'common.php';
-$delay = 10; //minutes
-$meters = getMeters();
-$meterId;
-$meterUrl;
-$count =1;   // meter loop counter
-$successCount = 0;
+
+
+ $apiUrl="http://192.168.1.64/ISAPI/AccessControl/AcsEvent?format=json";
+$username = "admin";
+$password = "hik12345";
 date_default_timezone_set("Asia/Dhaka");
 
- $current_date = date("Y-m-dH:i" , strtotime("-$delay minutes")) ;
+ $current_date = date("Y-m-dH:i" ) ;
 
 
-foreach ($meters as $meter) {     //$meters as $meter
+ $post = '{
+    "AcsEventCond": {
+        "searchID": "3166590d-cdb3-43f3-b25e-f6e98a05d359",
+        "searchResultPosition": 0,
+        "maxResults": 1000,
+        "major": 0,
+        "minor": 0,
+        "startTime": "2022-02-03T00:00:00+06:00",
+        "endTime": "2022-02-04T16:18:47+06:00",
+        "thermometryUnit":"celcius",
+        "currTemperature":1
+    }
+}';
 
-    
-   $meterId =  $meter['METER_ID'];
-   $meterUrl = $meter['METER_URL'];
-   //echo $meterId."".$meterUrl."<br>";
+// echo  $post; 
+// $data = json_decode($post);
+// print_r($data) ; 
 
-      echo "====="."<br>".$count."<br>"."====="."<br>";
-         
+   $client = curl_init($apiUrl);
+
+   
+
+    curl_setopt($client, CURLOPT_URL, $apiUrl);
+
+            // //Specify the username and password using the CURLOPT_USERPWD option.
+      curl_setopt($client, CURLOPT_USERPWD, $username . ":" . $password);       
+     curl_setopt($client, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+        curl_setopt($client, CURLOPT_POST, 1);
+
+         curl_setopt($client, CURLOPT_POSTFIELDS, $post);
+
+
+      // //Tell cURL to return the output as a string instead
+      // //of dumping it to the browser.
+        curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+          //  curl_setopt($client, CURLOPT_TIMEOUT, 15);
+       
+
+        $response = curl_exec($client);
+               
+      // curl_close($client);
+        echo "response :".$response ." length : ".strlen($response)."<br>"; 
         
 
-       fetchDataAndInsert($meterId ,$meterUrl,$current_date   ) ;
+         curl_close($client);
+         
+ //          print_r($response);
+ //       echo "<br>";
+ // printf($response);
+ //  echo "<br>";    
+ // print($response);
+   //      //  echo "<br>"; 
+   //   if($response == null){
 
-      $count++;
-
- }
-
-function fetchDataAndInsert(  $meterId , $meterUrl, $current_date ){
-            $username="mnj_ho_it";
-            $password ="123456";
-
-           // echo $apiUrl ;
-           // echo "<br>"; 
-             global $successCount;
-             global $delay;
-                  $arrayLength =0;
-
-                   $readingCount = 1;
-              $apiUrl = getBmsApiUrl($meterUrl , $current_date, $readingCount);
-              echo $apiUrl."<br>";  
-              
-
-              $array = getDataFromApi ($apiUrl,$username, $password );
-               
-
-               if ($array == "Access denied"){
-                 
-                   echo "No Data or access Denied <br>";
-                   
-                     $seq =  date('H', strtotime($current_date ));
-                     $seq = $seq*100;
-                     $seq =$seq  +date('i', strtotime($current_date ));
-                   
-                       $status=$array ;
-                      insertApiDataIntoTrendLogTable($meterId,$Timestamp,$value,$sequence, $delay , $status);
-                  
-                   return;
-
-               } 
-
-              $arrayLength = sizeof($array);
-
-
-             echo  " arrayLength :".$arrayLength."<br>";
-
-               
-
-               
-            if(   isset ($array['Row'] )   ){
-                    
-                    print_r($array);
-                   echo "<br>";
-
-                    $successCount++;
-                    echo "Success Count :".$successCount."<br>";
-
-                   
-                  $row = $array['Row'] ;
-
-                  $Timestamp= $row['Timestamp'] ;
-                  $value= $row['Value'];
-                  $sequence = $row['Seq'];
-                  $status= "";
-                  
-                 insertApiDataIntoTrendLogTable($meterId,$Timestamp,$value,$sequence, $delay , $status);
-                     
-               }
-
-
-             echo "==========================================================================================================================="."<br>" ;
+   //   // echo "response is null <br>"; 
+   //   // $response  = "NUll";
+   //   //  return  $response ;
+   //   }
+   //   else if ($response == "Access denied"){
+   //    // echo "response is Access Denied <br>";
+   //    //  $response  = "Access denied";
+   //  //   return  $response ;
     
-}
+   //    }
+   //   else if ($response == ""){
+   //    // echo "response is No data <br>";
+   //    //  $response  = "No data";
+   //  //   return  $response ;
 
 
 
