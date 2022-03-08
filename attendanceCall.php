54b8afdbdd9421a2e3b084888b24b2e092050a0e
sadmanfatin <sadmanfatin208@gmail.com>
2022-03-08 10:37:36 +0600

    Update attendanceCall.php

--- attendanceCall.php
+++ attendanceCall.php
@@ -3,8 +3,8 @@ include 'dbConn.php';
 include 'common.php';
 
 $scriptStartTime = date("Y-m-d H:m:s");
-echo "script last started at ".$scriptStartTime;
-echo "<br>";
+echo " ===========================================================   script last started at ".$scriptStartTime."\n";
+//echo "<br>";
 
 set_time_limit(0);
 $terminalLastEntryTimeArray = array();
